9a2b6ae6a579365c009c6b4873cd613d1eb92389
L-and <ksw1812@naver.com>
2022-05-02 17:10:54 +0900

    코드규칙변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,35 +13,36 @@ public class PlayerController : MonoBehaviour
     float distance = 1000.0f;
 
     /// 입력 변수들 ///
-    bool isRightMouseDown; // 우클릭 입력
+    bool bRightMouseDown; // 우클릭 입력
 
     /// 컴포넌트 변수들 ///
-    Rigidbody RGD; // 리지드바디
+    Rigidbody rigid; // 리지드바디
 
 
     private void Start()
     {
         /// 컴포넌트변수 할당 ///
-        RGD = GetComponent<Rigidbody>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         PlayerInput(); // 플레이어 입력을 받음
-        SetTargetPosition();
-        MovePlayerPosition();
+
+        SetTargetPosition(); // 이동할 목표포지션을 정함
+        MovePlayerPosition(); // 플레이어를 이동시킴
     }
 
 
     // 플레이어 입력을 받는하는 함수
     public void PlayerInput()
     {
-        isRightMouseDown = Input.GetMouseButtonDown(1); // 우클릭
+        bRightMouseDown = Input.GetMouseButtonDown(1); // 우클릭
     }
 
     public void SetTargetPosition() // 우클릭시 플레이어가 이동할 위치를 표시해줌
     { 
-        if(isRightMouseDown) // 우클릭이 입력됐을 때
+        if(bRightMouseDown) // 우클릭이 입력됐을 때
         {
             Ray ray = mainCam.ScreenPointToRay(Input.mousePosition); // 카메라에서 레이저를 쏨
             Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1f); // [디버그]레이저를 씬에 표시
