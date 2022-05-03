1a3d6e29fe482cbc4bc576aa0be44e49628ef437
L-and <ksw1812@naver.com>
2022-05-04 02:36:32 +0900

    적 이동 구현
    플레이어가 길막하면 길막되는문제가 있음

--- Assets/Scripts/System/EnemyMove.cs
+++ Assets/Scripts/System/EnemyMove.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 게임오브젝트 변수들 ///
+    GameObject mainTargetObject; // 적이 최종목표로하는 오브젝트
+
+    /// 이동 변수들 ///
+    Vector3 mainTargetPos; // 적이 목표로하는 위치
+
+    /// 컴포넌트 변수들 ///
+    NavMeshAgent agent;
+
+    private void Awake()
     {
+        /// 컴포넌트변수 할당 ///
+        agent = GetComponent<NavMeshAgent>();
+        mainTargetObject = GameObject.FindGameObjectWithTag("EndPoint");
+        mainTargetPos = mainTargetObject.transform.position;
+        print(mainTargetPos);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        move();
+    }
+
+    private void move()
+    {
+        // targetPos로 적의 위치를 이동
+        if (mainTargetPos != Vector3.zero)
+        {
+            agent.SetDestination(mainTargetPos);
+        }
     }
 }
--- Assets/Scripts/System/EnemySpawner.cs
+++ Assets/Scripts/System/EnemySpawner.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] enemyPrefabs; // 적 프리팹 리스트
+    Vector3 spawnPoint; // 적 생성 위치
+
+    private void Awake()
     {
-        
+        Vector3 spawnPoint = transform.position; // 적생성위치 지정
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EnemySpawn() // 적생성
     {
-        
+        print("적 생성!");
+        Instantiate(enemyPrefabs[0], transform); // 적프리팹 생성
     }
 }
